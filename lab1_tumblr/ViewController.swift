097d81f19613f38b9ce5a59c510c67c306650123
Jamila Duke-Smith <jamilads@jamilas-macbook-pro.local>
2018-09-11 20:50:24 -0400

    Initial Commit

---
+++ lab1_tumblr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab1_tumblr
+//
+//  Created by Jamila Duke-Smith on 9/11/18.
+//  Copyright © 2018 Jamila Duke-Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
